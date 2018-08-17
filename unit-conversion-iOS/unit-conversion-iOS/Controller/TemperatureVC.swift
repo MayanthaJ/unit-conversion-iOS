16fe3b69543c523e86504e86d5d282a52a1da720
MayanthaJ <mayantha_j@yahoo.co.uk>
2018-08-17 11:23:21 +0530

    Temperature model added

--- unit-conversion-iOS/unit-conversion-iOS/Controller/TemperatureVC.swift
+++ unit-conversion-iOS/unit-conversion-iOS/Controller/TemperatureVC.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class TemperatureVC: UIViewController , UITextFieldDelegate{
 
+    @IBOutlet weak var celciusTxt: UITextField!
+    @IBOutlet weak var fahrenheitTxt: UITextField!
+    @IBOutlet weak var kelvinTxt: UITextField!
+    
+    //convertion types
+    let celcius = "celcius"
+    let fahrenheit = "fahrenheit"
+    let kelvin = "kelvin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.celciusTxt.delegate = self
+        self.fahrenheitTxt.delegate = self
+        self.kelvinTxt.delegate = self
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,6 +38,66 @@ class TemperatureVC: UIViewController , UITextFieldDelegate{
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
+    
+    @IBAction func celciusConvert(_ sender: Any) {
+        if let celciusTxt = celciusTxt.text {
+            if let celciusW = Double(celciusTxt){
+                setToTextBoxes(temperature: celciusW, inputTempType: celcius)
+            }
+        }
+    }
+    
+    @IBAction func fahrenheitConvert(_ sender: Any) {
+        if let fahrenheitTxt = fahrenheitTxt.text {
+            if let fahrenheitW = Double(fahrenheitTxt){
+                setToTextBoxes(temperature: fahrenheitW, inputTempType: fahrenheit)
+            }
+        }
+    }
+    
+    @IBAction func kelvinConvert(_ sender: Any) {
+        if let kelvinTxt = kelvinTxt.text {
+            if let kelvinW = Double(kelvinTxt){
+                setToTextBoxes(temperature: kelvinW, inputTempType: kelvin)
+            }
+        }
+    }
+    
+    @IBAction func clearCelciusOnClick(_ sender: Any) {
+        clearTextBoxes()
+    }
+    
+    @IBAction func clearFahrenheitOnClick(_ sender: Any) {
+        clearTextBoxes()
+    }
+    
+    @IBAction func clearKelvinOnClick(_ sender: Any) {
+        clearTextBoxes()
+    }
+    
+    //main functions that handles temperature convertion class and set converted values to the textfields
+    func setToTextBoxes(temperature : Double , inputTempType :  String){
+        
+        let tempC = Temperature()
+        
+        if inputTempType == celcius{
+            fahrenheitTxt.text = "\(tempC.convertMain(fromType: celcius, toType: fahrenheit, temperature: temperature))"
+            kelvinTxt.text = "\(tempC.convertMain(fromType: celcius, toType: kelvin, temperature: temperature))"
+        }else if inputTempType == fahrenheit{
+            celciusTxt.text = "\(tempC.convertMain(fromType: fahrenheit, toType: celcius, temperature: temperature))"
+            kelvinTxt.text = "\(tempC.convertMain(fromType: fahrenheit, toType: kelvin, temperature: temperature))"
+        }else if inputTempType == kelvin{
+            celciusTxt.text = "\(tempC.convertMain(fromType: kelvin, toType: celcius, temperature: temperature))"
+            fahrenheitTxt.text = "\(tempC.convertMain(fromType: kelvin, toType: fahrenheit, temperature: temperature))"
+        }
+    }
+    
+    //function that clears all textfeilds
+    func clearTextBoxes(){
+        celciusTxt.text = ""
+        fahrenheitTxt.text = ""
+        kelvinTxt.text = ""
+    }
+    
 }
 
--- unit-conversion-iOS/unit-conversion-iOS/Model/Temperature.swift
+++ unit-conversion-iOS/unit-conversion-iOS/Model/Temperature.swift
@@ -8,5 +8,70 @@
 
 import Foundation
 class Temperature{
+    //Temp variables
+    var returnTemperature : Double = 0.00
+    var tempCelciusValue : Double = 0.00
     
+    //convertion types
+    let celcius = "celcius"
+    let fahrenheit = "fahrenheit"
+    let kelvin = "kelvin"
+    
+    //function to convert celcius to fahrenheit
+    func convertCelciusToFahrenheit( temperature : Double) -> Double{
+        returnTemperature = (temperature * 1.8) + 32
+        return (returnTemperature)
+    }
+    //function to convert celcius to kelvin
+    func convertCelciusToKelvin( temperature : Double) -> Double{
+        returnTemperature = temperature + 273.15
+        return (returnTemperature)
+    }
+    
+    //function to other types to celcius
+    func convertToCelcius(fromType : String , temperature : Double) -> Double{
+        if fromType == fahrenheit{
+            returnTemperature =  (temperature - 32) * 0.5556
+        }else if fromType == kelvin{
+            returnTemperature =  temperature - 273.15
+        }
+        
+        return (returnTemperature)
+    }
+    
+    //main function that returns all converion values
+    func convertMain(fromType : String , toType : String , temperature : Double) -> Double{
+        
+        if (fromType == celcius && toType == fahrenheit) {
+            tempCelciusValue = convertCelciusToFahrenheit(temperature: temperature)
+            returnTemperature =  tempCelciusValue
+            
+        }else if (fromType == celcius && toType == kelvin) {
+            tempCelciusValue = convertCelciusToKelvin(temperature: temperature)
+            returnTemperature =  tempCelciusValue
+            
+        }else if (fromType == fahrenheit && toType == celcius) {
+            
+            tempCelciusValue = convertToCelcius(fromType: fahrenheit, temperature: temperature)
+            returnTemperature =  tempCelciusValue
+            
+        }else if (fromType == fahrenheit && toType == kelvin) {
+            
+            tempCelciusValue = convertToCelcius(fromType: fahrenheit, temperature: temperature)
+            returnTemperature =  convertCelciusToKelvin(temperature: tempCelciusValue)
+            
+        }else if (fromType == kelvin && toType == celcius) {
+            
+            tempCelciusValue = convertToCelcius(fromType: kelvin, temperature: temperature)
+            returnTemperature =  tempCelciusValue
+            
+        }else if (fromType == kelvin && toType == fahrenheit) {
+            
+            tempCelciusValue = convertToCelcius(fromType: kelvin, temperature: temperature)
+            returnTemperature =  convertCelciusToFahrenheit(temperature: tempCelciusValue)
+            
+        }
+        
+        return (returnTemperature)
+    }
 }
